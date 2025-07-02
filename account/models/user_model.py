a401abe31b55a5a18c037dcd94ea79dae2a90b0e
dmarsanic2025 <dominik.marsanic@predavaci.algebra.hr>
2025-07-02 20:24:55 +0200

    272025

--- account/models/user_model.py
+++ account/models/user_model.py
@@ -7,14 +7,20 @@ from django.contrib.auth.models import (
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, password=None, extra_fields=None):
+    def create_user(self, email, password=None, **extra_fields):
         if not email:
             raise ValueError("The Email field must be set")
         email = self.normalize_email(email)
-        user = self.model(email=email, extra_fields=extra_fields)
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        return self.create_user(email, password, **extra_fields)
 
 
 class User(AbstractBaseUser, PermissionsMixin, models.Model):
---
+++ settings/models/__init__.py
@@ -0,0 +1,3 @@
+from .customer_types import CustomerType
+from .genders import Gender
+from .tenants import Tenant
